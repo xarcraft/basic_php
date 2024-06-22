5cf48de30ad8fe36158e0faa72dd64cc5d8f3ef3
Harold Gómez <xagobo14@gmail.com>
2024-06-22 15:50:46 -0500

    mejoras para el archivo incial

--- index.php
+++ index.php
@@ -1,29 +1,34 @@
-<!-- llamadas a api desde php -->
 <?php
+//Activar tipos estrictos
+declare(strict_types=1); // esta configuracion se debe poner a nivel de archivo NO GLOBAL y en la parte superior del script
+
 const API_URL = "https://whenisthenextmcufilm.com/api";
-# Inicializar una nueva sesion de cURL; ch= cURL handle
-$ch = curl_init(API_URL);
-// quitar verificador ssl para prevenir errores en ejecucion
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// Indicar que queremos recibir el resultado de la peticion y no mostrarlo en pantalla
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-/* ejecutar la peticion y guardar el resultado */
-$result = curl_exec($ch);
-/*
-    podemos optar por una alternativa que nos devuelve el json mucho mas rapido si solo quieres hacer el GET de una API y esta seria usar el file_get_contents
-    $result=file_get_contents(API_URL);
-*/
-/* comprobar y descartar errores en la conexion */
-if (curl_errno($ch)) {
-    $error_msg = curl_error($ch);
-    echo "Error al conectarse a la API $error_msg <br>";
-} else {
-    /* decodificar la informacion que devuelve la consulta */
+
+# para metodos y funciones es recomendable usar el snake_case
+function get_data(string $url): array
+{
+    # podemos optar por una alternativa que nos devuelve el json mucho mas rapido si solo quieres hacer el GET de una API y esta seria usar el file_get_contents
+    $result = file_get_contents($url);
     $data = json_decode($result, true);
-    // cerrar la conexion
-    curl_close($ch);
+    return $data;
 }
+
+function get_until_message(int $days): string
+{
+    return match (true) {
+        $days === 0 => "Hoy es el gran día",
+        $days === 1 => "Mañana es el gran día",
+        $days < 7 => "Ya solo falta una semana para el estreno",
+        $days < 30 => "este mes es el estreno",
+        default => "Ya solo quedan $days días para el estreno"
+    };
+}
+
+$data = get_data(API_URL);
+$untilMessage = get_until_message($data["days_until"]);
+
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -41,7 +46,7 @@ if (curl_errno($ch)) {
             <img src="<?= $data['poster_url']; ?>" alt="Poster de <?= $data['title']; ?>" width="300" style="border-radius: 16px;">
         </section>
         <hgroup>
-            <h3><?= $data['title']; ?> se estrena en <?= $data['days_until']; ?> días</h3>
+            <h3><?= $data['title']; ?> <?= $untilMessage; ?></h3>
             <p>Fecha de estreno: <?= $data['release_date']; ?></p>
             <p>La siguiente es: <?= $data['following_production']['title']; ?></p>
         </hgroup>
